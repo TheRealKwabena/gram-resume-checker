6934396d12a954048341ac5b2ffc7e3aef74c2d8
Honore Jabiro <honorejabiro@gmail.com>
2024-11-23 13:52:48 -0600

    edit home page

--- resume-checker/src/App.jsx
+++ resume-checker/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/Home'; 
 
 function App() {
 
+  const router = createBrowserRouter(createRoutesFromElements(
+    <>
+      <Route path="home" />
+    </>
+  ))
   return (
     <div>
-      
+      <HomePage />
     </div>
   )
 }
--- resume-checker/src/pages/Home.jsx
+++ resume-checker/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "../styles/HomePage.css"
 
-const Home = () => {
+function HomePage() {
+  const [resume, setResume] = useState(null);
+
+  const handleResumeUpload = (e) => {
+    setResume(e.target.files[0]);
+  };
+
+  const handleUploadClick = () => {
+    // Handle file upload (send to backend)
+  };
+
   return (
-    <div>
-      hello
+    <div className="homepage">
+      <h1>Resume Scanner</h1>
+      <p>Upload your resume to see how well it matches the job description!</p>
+      <input type="file" onChange={handleResumeUpload} />
+      <button onClick={handleUploadClick} disabled={!resume}>Upload Resume</button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default HomePage;
+
